ced6a59a889f369fa0ff5fdd3e39e066dc638174
jacopomanenti01 <jacopomanenti01@gmail.com>
2026-03-06 23:24:08 +0100

    removed old files and added .env in gitignore

--- src/db/statics.py
+++ src/db/statics.py
@@ -1,8 +1,1 @@
-import pathlib
-
 DB_URL = "ws://localhost:8000/rpc"
-DATA_DIR = pathlib.Path(__file__).parent.parent / "data"
-MICA = DATA_DIR / "MICA.pdf"
-OLLAMA = 'llama3.2:1b'
-if __name__ == "__main__":
-    print(DATA_DIR)
