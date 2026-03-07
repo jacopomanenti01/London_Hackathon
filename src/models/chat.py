7f44d6faa6908288cd571b1ae87b11915b295936
jacopomanenti01 <jacopomanenti01@gmail.com>
2026-03-07 12:16:33 +0100

    added wrappers for llm and embedder

---
+++ src/models/chat.py
@@ -0,0 +1,21 @@
+import os
+from langchain_openai import AzureChatOpenAI
+from ..utils.configs import LLM_MODEL, API_VERSION, AZURE_ENDPOINT
+API_KEY = os.getenv('API_KEY')
+
+
+llm = AzureChatOpenAI(
+    azure_endpoint=AZURE_ENDPOINT,
+    azure_deployment=LLM_MODEL,
+    api_version=API_VERSION,
+    api_key=API_KEY,
+    temperature=0,
+)
+
+def chat(message: str) -> str:
+    from langchain_core.messages import HumanMessage, SystemMessage
+    response = llm.invoke([
+        SystemMessage(content="You are a helpful assistant."),
+        HumanMessage(content=message),
+    ])
+    return response.content
---
+++ src/utils/configs.py
@@ -0,0 +1,4 @@
+AZURE_ENDPOINT = "https://genietrace-resource-eastus.cognitiveservices.azure.com"
+LLM_MODEL="gpt-5.4"
+EMBEDDING_MODEL="text-embedding-3-large"
+API_VERSION = "2025-04-01-preview"
